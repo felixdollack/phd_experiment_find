cc41ae2760f7c2b31235c0d1cc314b3afbdc8967
Felix Dollack <felix.dollack@googlemail.com>
2018-10-23 13:47:09 +0900

    Convert position and send to android
    This commit converts the vicon data to meters and updates the
    android client if position has changed.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -129,10 +129,14 @@ void ofApp::update(){
     this->_old_head_y = this->_head_y;
     this->_old_head_z = this->_head_z;
     this->_old_head_phi = this->_head_phi;
-    this->_head_x = this->_head_data.x_position - this->_x_origin;
-    this->_head_y = this->_head_data.y_position - this->_y_origin;
-    this->_head_z = this->_head_data.z_position - this->_z_origin;
-    this->_head_phi = fmod(this->_head_data.z_rotation - this->_phi_origin, 360.0f);
+    this->_head_x = (this->_head_data.x_position/1000) - this->_x_origin;
+    this->_head_y = (this->_head_data.y_position/1000) - this->_y_origin;
+    this->_head_z = (this->_head_data.z_position/1000) - this->_z_origin;
+    this->_head_phi = fmod(this->_head_data.z_rot_avg - this->_phi_origin, 360.0f);
+
+    if ((this->_old_head_x != this->_head_x) || (this->_old_head_y != this->_head_y) || (this->_old_head_z != this->_head_z) || (this->_old_head_phi != this->_head_phi)) {
+        sendMessageToPhone(0, "POSITION/" + ofToString(this->_head_x) + "/" + ofToString(this->_head_y) + "/" + ofToString(this->_head_z) + "/" + ofToString(this->_head_phi));
+    }
 }
 
 //--------------------------------------------------------------
@@ -251,9 +255,10 @@ void ofApp::toggleSound(const void *sender, bool &value) {
 }
 
 void ofApp::resetHeadOrigin() {
-    this->_x_origin = this->_head_data.x_position;
-    this->_y_origin = this->_head_data.y_position;
-    this->_phi_origin = this->_head_data.z_rotation;
+    this->_x_origin = this->_head_data.x_position/1000;
+    this->_y_origin = this->_head_data.y_position/1000;
+    this->_z_origin = this->_head_data.z_position/1000;
+    this->_phi_origin = this->_head_data.z_rot_avg;
 }
 
 ofVec2f ofApp::mapDistanceToPixel(ofVec2f pos) {
